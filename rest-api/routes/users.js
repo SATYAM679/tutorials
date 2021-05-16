28a8ca1900c19fe543a830096221377a65ead2ca
Satyam <679agarsatya@gmail.com>
2021-05-16 19:54:07 +0530

    sample users data added

--- rest-api/routes/users.js
+++ rest-api/routes/users.js
@@ -2,9 +2,26 @@ const express = require("express");
 
 const router = express.Router();
 
+const users = [
+  {
+    firstName: "Rick",
+    lastName: "Grimes",
+    role: "Leader",
+    zombieKillCount: 1010,
+    humanKillCount: 25,
+  },
+  {
+    firstName: "Negan",
+    lastName: "Smith",
+    role: "Leader",
+    zombieKillCount: 1610,
+    humanKillCount: 15,
+  },
+];
+
 // /users get route
 router.get("/users", (req, res) => {
-  res.send("all users");
+  res.send(users);
 });
 
 module.exports = router;
